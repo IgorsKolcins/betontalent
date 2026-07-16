5d5fdba0cf2f79f42944a70c8b77f5a02ff31622
Igors Kolčins <igors.kolcins+git@gmail.com>
2026-07-16 19:36:55 +0300

    Cache public blog pages at the CDN

--- src/routes/(public)/blog/+page.server.ts
+++ src/routes/(public)/blog/+page.server.ts
@@ -4,16 +4,23 @@ import { getLocaleSeo } from '$lib/seo';
 import { getLocale } from '$lib/paraglide/runtime.js';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ fetch, url }) => {
+export const load: PageServerLoad = async ({ fetch, setHeaders, url }) => {
 	const locale = getLocale();
 	const query = decodePostQuery(url.searchParams, 'blog');
+	const postsResult = await getPosts(fetch, {
+		...query,
+		locale
+	});
+
+	if (postsResult.ok) {
+		setHeaders({
+			'Cache-Control': 'public, max-age=120, s-maxage=86400'
+		});
+	}
 
 	return {
 		query,
 		seo: getLocaleSeo(url.origin, locale, '/blog'),
-		postsResult: await getPosts(fetch, {
-			...query,
-			locale
-		})
+		postsResult
 	};
 };
